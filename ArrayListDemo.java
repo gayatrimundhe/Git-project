9bf9063c5f87e781154a78af29c8226a7dfecbc3
gayatrimundhe <128040524+gayatrimundhe@users.noreply.github.com>
2023-03-17 11:29:47 +0530

    Add files via upload

---
+++ ArrayListDemo.java
@@ -0,0 +1,18 @@
+package com.example;
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	
+	public static void main(String[]args){
+		ArrayList<String>al=new ArrayList<String>();
+		al.add("shubham");
+		al.add("rahul");
+		al.add("laxman");
+		al.add("snehal");
+		al.add("kshitija");
+		
+		
+		System.out.println(al);
+		}
+
+}
